51b7dc0358ec23a4ce5d0c97aff5f7b6451d9d93
Gabriella <g.johnson4520@gmail.com>
2019-02-24 11:16:40 -0700

    Update screen.js

--- Project1/screen.js
+++ Project1/screen.js
@@ -14,7 +14,9 @@ window.addEventListener('orientationchange', changeOrientation, false);
 function changeOrientation() {
 if (orientation == 0 || orientation == 180) {
     document.getElementById('extra').style.display='none'
+    alert("You are now in portrait");
 }
 else {
     document.getElementById('extra').style.display='grid' 
+    alert("You are now in landscape");
 }}
