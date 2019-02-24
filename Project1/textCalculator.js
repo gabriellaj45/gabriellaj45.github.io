98b410515eb251248dbf647a0dedae057851ede6
Gabriella <g.johnson4520@gmail.com>
2019-02-24 16:07:39 -0700

    Keyboard Input

---
+++ Project1/textCalculator.js
@@ -0,0 +1,58 @@
+function getText() {
+    var equationString;
+    equationString = document.getElementById('mathEquation').value;
+
+    var equationPieces = equationString.split(" ");
+    var operator = equationPieces[0];
+    var firstNum = equationPieces[1];
+    var secondNum = equationPieces[2];
+    
+    operation = findOperator(operator);
+    answer = calculate(operation, firstNum, secondNum);
+    $("#currentValue").html(answer);
+    return false;
+    
+}
+
+function findOperator(operator){
+    
+    if(operator == '+') {
+        return 1
+    }
+    if(operator == '-') {
+        return 2
+    }
+    if(operator == '*') {
+        return 3
+    }
+    if(operator == '/') {
+        return 4
+    }
+}
+
+function calculate(op, first, second) {
+    if(op == 1){
+        var firstNum = parseInt(first);
+        var secondNum = parseInt(second);
+        var answer = firstNum + secondNum;
+        return answer
+    }
+    if(op == 2){
+        var firstNum = parseInt(first);
+        var secondNum = parseInt(second);
+        var answer = firstNum - secondNum;
+        return answer
+    }
+    if(op == 3){
+        var firstNum = parseInt(first);
+        var secondNum = parseInt(second);
+        var answer = firstNum * secondNum;
+        return answer
+    }
+    if(op == 4){
+        var firstNum = parseInt(first);
+        var secondNum = parseInt(second);
+        var answer = firstNum / secondNum;
+        return answer
+    }
+}
