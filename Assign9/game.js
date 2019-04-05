01fe121b0801167d14130cca5c714c9960e8e3e0
Gabriella <g.johnson4520@gmail.com>
2019-04-05 10:28:08 -0600

    Final Touches

--- Assign9/game.js
+++ Assign9/game.js
@@ -1,23 +1,24 @@
 $(document).ready(function() { // do this when the document is loaded
 	$("#sorryTab").show(); // show the element with ID "element"
     $("#monopolyTab").hide(); // hide the element with ID "otherElement"
-    $("#pokerTab").hide();
+    $("#genericTab").hide();
 });
 
 $("#monopolyButton").click(function() { // do this when the button is click
 	$("#monopolyTab").show(); // show the element with ID "element"
     $("#sorryTab").hide(); // hide the element with ID "otherElement"
-    $("#pokerTab").hide();
+    $("#genericTab").hide();
 });
 
-$("#pokerButton").click(function() { // do this when the button is click
-	$("#pokerTab").show(); // show the element with ID "element"
-    $("#sorryTab").hide(); // hide the element with ID "otherElement"
-    $("#monopolyTab").hide();
-});
 
 $("#sorryButton").click(function() { // do this when the button is click
 	$("#sorryTab").show(); // show the element with ID "element"
     $("#monopolyTab").hide(); // hide the element with ID "otherElement"
-    $("#listTab").hide();
+    $("#genericTab").hide();
+});
+
+$("#genericButton").click(function() { // do this when the button is click
+	$("#genericTab").show(); // show the element with ID "element"
+    $("#monopolyTab").hide(); // hide the element with ID "otherElement"
+    $("#sorryTab").hide();
 });
